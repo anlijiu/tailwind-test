fb3feb261a9b4ed3bbfc6370546fa150829744ab
anlijiu <anlijiu@hozonauto.com>
2023-05-04 16:34:45 +0800

    add grid test   g1.html  g2.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,106 +3,8 @@ module.exports = {
     // enabled: true,
     // content: ['./src/html/*.html'],
   ],
+
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      'black40a': 'rgba(0, 0, 0, 0.4)',
-      'grayLight': '#ababab',
-      'gray': '#4F4F4F',
-      'red': '#FC0000',
-      'blackSolid': '#000',
-      'black': '#333333',
-      'white': '#FFFFFF'
-    },
-    spacing: {
-      'gutter': '1.6rem',
-      '1/2': '50%',
-      '1/3': '33.33%',
-      300: '30rem',
-      240: '24rem',
-      200: '20rem',
-      160: '16rem',
-      100: '10rem',
-      80: '8rem',
-      64: '6.4rem',
-      40: '4rem',
-      32: '3.2rem',
-      24: '2.4rem',
-      16: '1.6rem',
-      8: '.8rem',
-      0: '0'
-    },
-    width: {
-      'desktop': '132rem',
-      'screen': '100vw',
-      'full': '100%',
-      '1/3': '33.33%',
-      '2/3': '66.66%',
-    },
-    height: {
-      '1': '1px',
-      '2': '2px',
-      '1/3': '33.33%',
-      '1/4': '25%',
-      '1/2': '50%',
-      '2/3': '66.66%',
-      'full': '100%',
-      'screen': '100vh',
-      700: '70rem',
-      600: '60rem',
-      500: '50rem',
-      400: '40rem',
-      360: '36rem',
-      300: '30rem',
-      200: '20rem',
-      120: '12rem',
-      80: '8rem',
-      64: '6.4rem',
-      32: '3.2rem',
-      24: '2.4rem'
-    },
-    flex: {
-      '0-0-100': '0 0 100%',
-      '0-0-16': '0 0 16.66%',
-      '0-0-25': '0 0 25%',
-      '0-0-33': '0 0 33.33%',
-      '0-0-50': '0 0 50%',
-      '0-0-auto': '0 0 auto',
-      1: '1 1 0%'
-    },
-    fontWeight: {
-      bold: '700',
-      medium: '500',
-      regular: '400',
-      light: '300',
-    },
-    borderWidth: {
-      '0': '0',
-      '1': '1px'
-    },
-    fontSize: {
-      '14': '1.4rem',
-      '16': '1.6rem',
-      '18': '1.8rem',
-      '24': '2.4rem',
-      '32': '3.2rem',
-      '48': '4.8rem',
-      '72': '7.2rem'
-    },
-    screens: {
-      lg: {
-        min: '1025px'
-      },
-      md: {
-        max: '1024px',
-        min: '768px'
-      },
-      sm: {
-        max: '767px'
-      }
-    },
-    extend: {},
-  },
   variants: {
     extend: {},
   },
@@ -119,4 +21,26 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
   ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+      },
+       gridColumnStart: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+      },
+      gridColumnEnd: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+      }
+    }
+  }
 }
